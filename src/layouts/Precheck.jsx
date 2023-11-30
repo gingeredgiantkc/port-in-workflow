93348729fe4ccada94bb08ca406dc5fcb0e064a5
gingeredgiantkc <68573208+gingeredgiantkc@users.noreply.github.com>
2023-11-30 08:19:27 -0600

    added new resource button

--- src/layouts/Precheck.jsx
+++ src/layouts/Precheck.jsx
@@ -4,7 +4,7 @@ import useAppStateContext from '../hooks/useAppStateContext'
 import Modal from '../components/Modal'
 import Button from '../components/buttons/Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft, faArrowRight, faArrowRotateLeft } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faArrowRight, faArrowRotateLeft, faBook } from '@fortawesome/free-solid-svg-icons'
 import styles from './Precheck.module.scss'
 
 export function Precheck() {
@@ -35,6 +35,11 @@ export function Precheck() {
         <div id='third' className={styles.band}></div>
         <img id="logo" src='./src/assets/frontier.svg' className={styles.logo} />
         <div id="window" className={styles.gridContainerII}>
+          <div id="help-btns" className={styles.gridItemI}>
+            <Button type="home" onClick={() => navigate('/')}>
+              <FontAwesomeIcon icon={faBook} inverse fixedWidth />
+            </Button>
+          </div>
           <div id="nav-btns" className={styles.gridItemI}>
             <Button type="reset" onClick={() => handleOpen()}>
               <FontAwesomeIcon icon={faArrowRotateLeft} inverse fixedWidth />
