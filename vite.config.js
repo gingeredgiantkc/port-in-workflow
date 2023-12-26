63c39bbd6a5eee50b2a8f321b711cc7ee8949bee
gingeredgiantkc <68573208+gingeredgiantkc@users.noreply.github.com>
2023-12-25 20:42:58 -0600

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: 'camelCaseOnly'
+  base: '/port-in-workflow/',
+  plugins: [react()],
+    css: {
+      modules: {
+        localsConvention: 'camelCaseOnly'
+      }
     }
   }
-})
+)
