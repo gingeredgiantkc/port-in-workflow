c46fabd08ea2c1655de8ad751ec527d005c0f1cf
gingeredgiantkc <68573208+gingeredgiantkc@users.noreply.github.com>
2023-12-28 07:01:21 -0600

    prepared for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/port-in-workflow/',
+  base: '/port-in-workflow/',
   plugins: [react()],
     css: {
       modules: {
